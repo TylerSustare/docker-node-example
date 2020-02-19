89ca66afb401568b27d9e2ad58aec49e3a8776b5
TylerSustare <tylersustare@gmail.com>
2020-02-19 10:36:05 -0800

    add lowdb. npm commands

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
-  const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+app.post("/songs", (req, res) => {
+  db.defaults({ songs: [] }).write();
+  res.send("spoon");
 });
 
-app.get('/test', (req, res) => {
-  console.log('Hello world received a request.');
-
-  res.send(`Hello Test`);
+app.get("/song", (req, res) => {
+  const value = db
+    .get("posts")
+    .find({ id: 1 })
+    .value();
+  res.status(200).send({ value });
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Hello world listening on port', port);
+  console.log("Hello world listening on port", port);
 });
